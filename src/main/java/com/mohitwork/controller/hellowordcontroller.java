07760d20a28f984b4d0fd0d2b7787dc7707a71dc
mohitkaremore4@gmail.com <mohitkaremore4@gmail.com>
2025-09-16 17:19:14 +0530

    final commit

--- src/main/java/com/mohitwork/controller/hellowordcontroller.java
+++ src/main/java/com/mohitwork/controller/hellowordcontroller.java
@@ -1,5 +1,13 @@
 package com.mohitwork.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hellowordcontroller {
+	@GetMapping("/message")
+	public String getmessage() {
+		return "hi from mohit";
+	}
 
 }
