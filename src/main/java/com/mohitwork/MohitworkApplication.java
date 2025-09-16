6f11f88ddb0c3f22213b2004f1dae8b82744088d
mohitkaremore4@gmail.com <mohitkaremore4@gmail.com>
2025-09-16 16:38:20 +0530

    first commit

---
+++ src/main/java/com/mohitwork/MohitworkApplication.java
@@ -0,0 +1,13 @@
+package com.mohitwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohitworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohitworkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mohitwork/controller/hellowordcontroller.java
@@ -0,0 +1,5 @@
+package com.mohitwork.controller;
+
+public class hellowordcontroller {
+
+}
